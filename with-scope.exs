f399b2f8594114c6003a173ab7ae796933d129c0
Art Mills <art@hamcois.com>
2016-01-22 18:29:29 +0000

    Up to Chapter 5. This is Elixir 1.2 not Phoenix

---
+++ with-scope.exs
@@ -0,0 +1,13 @@
+content = "Now is the time"
+
+result = with {:ok, file} = File.open("/etc/passwd"),
+  content = IO.read(file, :all),
+  :ok = File.close(file),
+  [_, uid, gid] <- Regex.run(~r/_lp:.*?:(\d+):(\d+)/, content)
+do
+  "Group: #{gid}, User: #{uid}"
+end
+
+IO.puts inspect(result)
+IO.puts content
+  
